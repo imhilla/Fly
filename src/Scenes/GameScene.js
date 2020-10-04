717db1dfb8e4075cd6ee2e1a20f1440a88a092ca
imhilla <hillaryodhiambo282@gmail.com>
2020-10-04 16:30:47 +0300

    add bullets

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -3,6 +3,9 @@ var spaceField;
 var backgroundV;
 var player;
 var cursors;
+var bullets;
+var bulletTime = 0;
+var fireButton;
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -20,7 +23,7 @@ export default class GameScene extends Phaser.Scene {
 
     this.load.image('starfield', 'assets/space.png');
     this.load.image('player', 'assets/rocket.png');
-
+    this.load.image('bullet', 'assets/bullet.png');
   }
 
   create() {
@@ -28,8 +31,17 @@ export default class GameScene extends Phaser.Scene {
     backgroundV = 5
     player = this.physics.add.sprite(400, 500, 'player');
     cursors = this.input.keyboard.createCursorKeys();
-    this.physics.world.bounds.width = groundLayer.width;
-    this.physics.world.bounds.height = groundLayer.height;
+    this.bullets = this.add.group();
+    this.bullets.children.each(function (bullet) {
+      bullet.enableBody = true;
+      bullet.physicsBodyType = Phaser.Physics.ARCADE
+      bullet.createMultiple(30, 'bullet')
+      bullet.set('anchor.x', 0.5)
+      bullet.set('anchor.y', 1)
+      bullet.set('outOfBoundKill', true)
+      bullet.set('checkWorldBounds', true)
+    }, this);
+    this.fireButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
   }
 
   update() {
@@ -43,5 +55,20 @@ export default class GameScene extends Phaser.Scene {
     if (cursors.right.isDown) {
       player.body.setVelocityX(350);
     }
+
+    if (this.fireButton.isDown) {
+      fireBullet();
+    }
   }
 };
+
+const fireBullet = () => {
+  if (this.time.now > this.bulletTime) {
+    this.bullet = this.bullets.getFirstExists(false)
+    // if (this.bullet) {
+    //   this.bullet.reset(player.x + 14, player.y);
+    //   this.bullet.body.velocity.y = -400
+    //   this.bulletTime = this.time.now + 200;
+    // }
+  }
+}
