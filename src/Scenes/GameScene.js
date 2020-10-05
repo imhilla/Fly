d048b70f22e59ba5a948624f97d7b6ff5f7e1ebb
imhilla <hillaryodhiambo282@gmail.com>
2020-10-05 18:35:40 +0300

    add woof animation

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -34,9 +34,8 @@ export default class GameScene extends Phaser.Scene {
     platforms = this.physics.add.staticGroup();
     platforms.enableBody = true
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
-    // platforms.create(750, 220, 'ground');
+    platforms.create(750, 220, 'ground');
 
     let ledge = platforms.create(400, 450, 'ground')
     ledge.body.immovable = true
@@ -45,17 +44,79 @@ export default class GameScene extends Phaser.Scene {
     ledge.body.immovable = true
 
     player = this.physics.add.sprite(100, 450, 'woof');
-    // game.physics.arcade.enable(player)
+    // this.physics.arcade.enable(player)
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
     player.body.gravity.y = 800
-  
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('woof', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'woof', frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('woof', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.physics.add.collider(player, platforms);
+
+    // player.animations.add('left', [0, 1], 10, true)
+    // player.animations.add('right', [2, 3], 10, true)
+    diamonds = this.add.group()
+    diamonds.enableBody = true
+
+    diamonds = this.physics.add.group({
+      key: 'diamond',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    // diamond.children.iterate(function (child) {
+    //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    // });
+
+    // for (var i = 0; i < 12; i++) {
+    //   const diamond = diamonds.create(i * 70, 0, 'diamond')
+
+    //   //  Drop em from the sky and bounce a bit
+    //   diamond.body.gravity.y = 1000
+    //   diamond.body.bounce.y = 0.3 + Math.random() * 0.2
+    // }
+    cursors = this.input.keyboard.createCursorKeys();
 
   }
 
   update() {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160);
 
+      player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+
+      player.anims.play('right', true);
+    }
+    else {
+      player.setVelocityX(0);
+
+      player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+      player.setVelocityY(-330);
+    }
   }
 
 };
