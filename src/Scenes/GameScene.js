6b817cb081b2cfc96d73b5ea043c45174f8b10a4
imhilla <hillaryodhiambo282@gmail.com>
2020-10-05 17:22:11 +0300

    create grounds

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -31,6 +31,14 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     this.add.sprite(400, 300, 'sky')
+    platforms = this.physics.add.staticGroup();
+    platforms.enableBody = true
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    // const ground = platforms.create(0, this.world.height - 64, 'ground')
 
   }
 
