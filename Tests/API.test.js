945176e03842831ed05471b8b2ac36e14ffe0ff4
imhilla <hillaryodhiambo282@gmail.com>
2020-10-08 12:13:13 +0300

    add tests for the API

---
+++ Tests/API.test.js
@@ -0,0 +1,37 @@
+import API from '../src/Objects/API';
+
+it('should return score of player', () => {
+  API.getScores()
+    .then((data) => {
+      expect(data).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            score: '100',
+          }),
+        ]),
+      );
+    })
+    .catch(() => { });
+});
+
+it('should return players username', () => {
+  API.getScores()
+    .then((data) => {
+      expect(data).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            user: 'Hillary Kiptoo',
+          }),
+        ]),
+      );
+    })
+    .catch(() => { });
+});
+
+it('should send an object to the API', () => {
+  API.postScores()
+    .then((data) => {
+      expect(typeof data).toBe('object');
+    })
+    .catch(() => { });
+});
